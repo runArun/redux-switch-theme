3964e6683e15aa8d2fce8c8994c74884630f6de6
runArun <zl2613185@qq.com>
2017-11-26 23:39:58 +1100

    dumb components

---
+++ src/react-redux.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react'
+
+export const connect = (mapStateToProps) => (WrappedComponent) => {
+    class Connect extends Component {
+        static contextTypes = {
+            store: PropTypes.object
+        }
+
+        render () {
+            const { store } = this.context
+            let stateProps = mapStateToProps(store.getState())
+            // {...stateProps} 意思是把这个对象里面的属性全部通过 `props` 方式传递进去
+            return <WrappedComponent {...stateProps} />
+        }
+    }
+
+    return Connect
+}
