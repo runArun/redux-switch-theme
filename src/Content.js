15daf0241f1699192e5c583a3a66106ae588fdf4
runArun <zl2613185@qq.com>
2017-11-29 10:53:42 +1100

    add connect !

--- src/Content.js
+++ src/Content.js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import ThemeSwitch from './ThemeSwitch'
-import PropTypes from 'prop-types'
+import { connect } from './react-redux'
+
 class Content extends Component {
-    static contextTypes = {
-        store: PropTypes.object // import PropTypes from 'prop-types'
-    }
-
-    constructor () {
-        super()
-        this.state = { themeColor: '' }
-    }
-
-    componentWillMount () {
-        const { store } = this.context
-        this._updateThemeColor()
-        store.subscribe(() => this._updateThemeColor())
-    }
-    //给 Header.js、Content.js、ThemeSwitch.js 的 componentWillMount 生命周期都加上监听数据变化重新渲染.
-    _updateThemeColor () {
-        const { store } = this.context
-        const state = store.getState()
-        this.setState({ themeColor: state.themeColor })
+    static propTypes = {
+        themeColor: PropTypes.string
     }
 
     render () {
         return (
             <div>
-                <p style={{ color: this.state.themeColor }}>React.js 小书内容</p>
+                <p style={{ color: this.props.themeColor }}>React.js 小书内容</p>
                 <ThemeSwitch />
             </div>
         )
     }
-}// render(){return()}
+}
+
+const mapStateToProps = (state) => {
+    return {
+        themeColor: state.themeColor
+    }
+}
+Content = connect(mapStateToProps)(Content)
 
 export default Content
